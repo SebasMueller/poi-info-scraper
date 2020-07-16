c694c3a542a7c40d0aef9cb06bd36dace41816fb
shadowbourne <shadowbourne3@gmail.com>
2020-07-16 18:00:06 +0100

    Second version
    Support for Sainsbury's, Tesco's, Morrison's, Aldi and Waitrose

--- POI.py
+++ POI.py
@@ -1,9 +1,9 @@
 import requests
+import html
 limit = 3
-radius = 1
+radius = 15
 lat = 52.824201
 lng = 1.389848
-
 def get_sainsburys_data(lat,lng):
     API_URL = "https://stores.sainsburys.co.uk/api/v1/stores/"
     params = {
@@ -66,17 +66,16 @@ def get_sainsburys_data(lat,lng):
     return hoursArray
 
 def get_tesco_data(lat, lng):
-    API_URL = "https://api.tesco.com/tescolocation/v3/locations/search?offset=0"
-    params =    { 'limit' : limit,
-                'sort' : "near:%22{0},{1}%22".format(lat,lng),
-                'filter' : "category:Store%20AND%20isoCountryCode:x-uk",
+    API_URL = "https://api.tesco.com/tescolocation/v3/locations/search"
+    params = {  'offset' : 0,
+                'limit' : limit,
+                'sort' : 'near:"{0},{1}"'.format(lat,lng),
+                'filter' : "category:Store AND isoCountryCode:x-uk",
                 'fields' : "name,geo,openingHours"
                 #known fields: "name,geo,openingHours,altIds.branchNumber,contact,facilities"
-                }
+             }
     headers = {"x-appkey": "store-locator-web-cde"}
-    # rq = requests.get(API_URL, params=params, headers=headers)
-    #(FOR SOME REASON THIS ^ LINE CAUSES ISSUES SO HAVE REPLACED WITH THE BELOW MANUAL LINE RATHER THAN USING THE PARAMS ARGUMENT OF requests.get())
-    rq = requests.get(API_URL + "&limit={0}&sort={1}&filter={2}&fields={3}".format(params['limit'], params['sort'], params['filter'], params['fields']), headers=headers)
+    rq = requests.get(API_URL, params=params, headers=headers)
     if rq.status_code != 200:
         print(rq.json())
         return False
@@ -92,23 +91,351 @@ def get_tesco_data(lat, lng):
     openingHours = res[i]['location']['openingHours'][0]['standardOpeningHours']
     dayKeys = { 'mo' : 'Monday', 'tu' : 'Tuesday', 'we' : 'Wednesday', 'th' : 'Thursday', 'fr' : 'Friday', 'sa' : 'Saturday', 'su' : 'Sunday'}
     hoursArray = []
-    for key in openingHours:
-
-        if openingHours[key]['isOpen'] == 'true':
-            actualHours = {'open' : openingHours[key]['open'][:2] + ":" + openingHours[key]['open'][2:],
-                        'close' : openingHours[key]['close'][:2] + ":" + openingHours[key]['close'][2:]}
+    for key in ['mo','tu','we','th','fr','sa','su']:
+        try:
+            if openingHours[key]['isOpen'] == 'true':
+                actualHours = {'open' : openingHours[key]['open'][:2] + ":" + openingHours[key]['open'][2:],
+                            'close' : openingHours[key]['close'][:2] + ":" + openingHours[key]['close'][2:]}
+                keyHours = {'day' : dayKeys[key],
+                        'open' : True,
+                        'hours' : [actualHours]
+                        }
+            else:
+                keyHours = {'day' : dayKeys[key],
+                        'open' : False}
+        except:
             keyHours = {'day' : dayKeys[key],
-                    'open' : True,
-                    'hours' : [actualHours]
-                    }
-        else:
-            keyHours = {'day' : dayKeys[key],
-                    'open' : False}
+                        'open' : False}
 
         hoursArray.append(keyHours)
     return hoursArray
 
-print(get_sainsburys_data(lat,lng))
+def get_morrisons_data(lat, lng):
+    apikey = "kxBdM2chFwZjNvG2PwnSn3sj6C53dLEY"
+    API_URL = "https://api.morrisons.com/location/v2//stores"
+    params = { 'apikey' : apikey,
+                'distance' : radius*1000,
+                'lat' : lat,
+                'lon' : lng,
+                'limit' : limit,
+                'offset' : 0,
+                'storeformat' : "supermarket"
+             }
+    rq = requests.get(API_URL, params=params)
+    if rq.status_code != 200:
+        print(rq.json())
+        return False
+    try:
+        res = rq.json()["stores"]
+        if len(res) == 0:
+            return False
+    except:
+        return False
+    i=0
+    #INSERT POTENTIAL CHECK THAT STORE MATCHES DESIRED STORENAME!! (e.g. "Morrisons" + res[0]["storename"])
+    #e.g.:
+    # while True:
+    #     if name != "Morrisons" + res[i]["storename"]:
+    #         i += 1
+    #     else:
+    #         break
+    openingHours = res[i]['openingTimes']
+    dayKeys = { 'mon' : 'Monday', 'tue' : 'Tuesday', 'wed' : 'Wednesday', 'thu' : 'Thursday', 'fri' : 'Friday', 'sat' : 'Saturday', 'sun' : 'Sunday'}
+    hoursArray = []
+    for key in ['mon','tue','wed','thu','fri','sat','sun']:
+        try:
+            actualHours = {'open' : openingHours[key]['open'][:5],
+                        'close' : openingHours[key]['close'][:5]}
+            keyHours = {'day' : dayKeys[key],
+                    'open' : True,
+                    'hours' : [actualHours]
+                    }
+        except:
+            keyHours = {'day' : dayKeys[key],
+                        'open' : False}
+
+        hoursArray.append(keyHours)
+    return hoursArray
+
+def get_waitrose_data(lat, lng):
+    API_URL_locations = "https://www.waitrose.com/shop/NearestBranchesCmd"
+    API_URL_hours = "https://www.waitrose.com/shop/StandardWorkingHoursView"
+    params_locations = {'latitude' : lat,
+                        'longitude' : lng,
+                        }
+    rq = requests.get(API_URL_locations, params=params_locations)
+    if rq.status_code != 200:
+        print(rq.json())
+        return False
+    try:
+        res = rq.json()
+        if not res['success'] or len(res['branchList']) == 0:
+            return False
+        res = res["branchList"]
+    except:
+        return False
+    i=0
+    #INSERT POTENTIAL CHECK THAT STORE MATCHES DESIRED STORENAME!! (e.g. "Waitrose" + res[0]["branchName"])
+    #e.g.:
+    # while True:
+    #     if name != "Waitrose" + res[i]["branchName"]:
+    #         i += 1
+    #     else:
+    #         break
+    branchId = res[i]['branchId']
+    params_hours = { 'branchId' : branchId}
+    rq = requests.get(API_URL_hours, params=params_hours)
+    hoursStringHTML = rq.text
+    import re
+    cleanr = re.compile('<.*?>|\s')
+    hoursString = re.sub(cleanr, '', hoursStringHTML)
+    #This section is to ensure if the shop is closed, correct times are displayed
+    sundayIndex = hoursString.find("Sunday:")
+    mondayIndex = hoursString.find("Monday:")
+    tuesdayIndex = hoursString.find("Tuesday:")
+    wednesdayIndex = hoursString.find("Wednesday:")
+    thursdayIndex = hoursString.find("Thursday:")
+    fridayIndex = hoursString.find("Friday:")
+    saturdayIndex = hoursString.find("Saturday:")
+
+    hoursArray = []
+    #equivalent of the usual for loop V
+    if mondayIndex == -1:
+        keyHours = {'day' : "Monday",
+                        'open' : False}
+        hoursArray.append(keyHours)
+    else:
+        try:
+            day = hoursString[mondayIndex+7:tuesdayIndex]
+            if day.find("los") != -1: #check if the world close or Close or closed or Closed or Closure etc.. appears
+                keyHours = {'day' : "Monday",
+                        'open' : False}
+                hoursArray.append(keyHours)
+            firstDigitIndex = [x.isdigit() for x in day].index(True)
+            actualHours = { 'open' : day[firstDigitIndex:firstDigitIndex+5],
+                            'close' : day[firstDigitIndex+6:firstDigitIndex+11]}
+            keyHours = {'day' : "Monday",
+                        'open' : True,
+                        'hours' : actualHours}
+            hoursArray.append(keyHours)
+        except:
+            keyHours = {'day' : "Monday",
+                        'open' : False}
+            hoursArray.append(keyHours)
+            
+
+    if tuesdayIndex == -1:
+        keyHours = {'day' : "Tuesday",
+                        'open' : False}
+        hoursArray.append(keyHours)
+    else:
+        try:
+            day = hoursString[tuesdayIndex+8:wednesdayIndex]
+            if day.find("los") != -1: #check if the world close or Close or closed or Closed or Closure etc.. appears
+                keyHours = {'day' : "Tuesday",
+                        'open' : False}
+                hoursArray.append(keyHours)
+            firstDigitIndex = [x.isdigit() for x in day].index(True)
+            actualHours = { 'open' : day[firstDigitIndex:firstDigitIndex+5],
+                            'close' : day[firstDigitIndex+6:firstDigitIndex+11]}
+            keyHours = {'day' : "Tuesday",
+                        'open' : True,
+                        'hours' : actualHours}
+            hoursArray.append(keyHours)
+        except:
+            keyHours = {'day' : "Tuesday",
+                        'open' : False}
+            hoursArray.append(keyHours)
+    if wednesdayIndex == -1:
+        keyHours = {'day' : "Wednesday",
+                        'open' : False}
+        hoursArray.append(keyHours)
+    else:
+        try:
+            day = hoursString[wednesdayIndex+10:thursdayIndex]
+            if day.find("los") != -1: #check if the world close or Close or closed or Closed or Closure etc.. appears
+                keyHours = {'day' : "Wednesday",
+                        'open' : False}
+                hoursArray.append(keyHours)
+            firstDigitIndex = [x.isdigit() for x in day].index(True)
+            actualHours = { 'open' : day[firstDigitIndex:firstDigitIndex+5],
+                            'close' : day[firstDigitIndex+6:firstDigitIndex+11]}
+            keyHours = {'day' : "Wednesday",
+                        'open' : True,
+                        'hours' : actualHours}
+            hoursArray.append(keyHours)
+        except:
+            keyHours = {'day' : "Wednesday",
+                        'open' : False}
+            hoursArray.append(keyHours)
+        
+    if thursdayIndex == -1:
+        keyHours = {'day' : "Thursday",
+                        'open' : False}
+        hoursArray.append(keyHours)
+    else:
+        try:
+            day = hoursString[thursdayIndex+9:fridayIndex]
+            if day.find("los") != -1: #check if the world close or Close or closed or Closed or Closure etc.. appears
+                keyHours = {'day' : "Thursday",
+                        'open' : False}
+                hoursArray.append(keyHours)
+            firstDigitIndex = [x.isdigit() for x in day].index(True)
+            actualHours = { 'open' : day[firstDigitIndex:firstDigitIndex+5],
+                            'close' : day[firstDigitIndex+6:firstDigitIndex+11]}
+            keyHours = {'day' : "Thursday",
+                        'open' : True,
+                        'hours' : actualHours}
+            hoursArray.append(keyHours)
+        except:
+            keyHours = {'day' : "Thursday",
+                        'open' : False}
+            hoursArray.append(keyHours)
+    if fridayIndex == -1:
+        keyHours = {'day' : "Friday",
+                        'open' : False}
+        hoursArray.append(keyHours)
+    else:
+        try:
+            day = hoursString[fridayIndex+7:saturdayIndex]
+            if day.find("los") != -1: #check if the world close or Close or closed or Closed or Closure etc.. appears
+                keyHours = {'day' : "Friday",
+                        'open' : False}
+                hoursArray.append(keyHours)
+            firstDigitIndex = [x.isdigit() for x in day].index(True)
+            actualHours = { 'open' : day[firstDigitIndex:firstDigitIndex+5],
+                            'close' : day[firstDigitIndex+6:firstDigitIndex+11]}
+            keyHours = {'day' : "Friday",
+                        'open' : True,
+                        'hours' : actualHours}
+            hoursArray.append(keyHours)
+        except:
+            keyHours = {'day' : "Friday",
+                        'open' : False}
+            hoursArray.append(keyHours)
+    if saturdayIndex == -1:
+        keyHours = {'day' : "Saturday",
+                        'open' : False}
+        hoursArray.append(keyHours)
+    else:
+        try:
+            day = hoursString[saturdayIndex+9:]
+            if day.find("los") != -1: #check if the world close or Close or closed or Closed or Closure etc.. appears
+                keyHours = {'day' : "Saturday",
+                        'open' : False}
+                hoursArray.append(keyHours)
+            firstDigitIndex = [x.isdigit() for x in day].index(True)
+            actualHours = { 'open' : day[firstDigitIndex:firstDigitIndex+5],
+                            'close' : day[firstDigitIndex+6:firstDigitIndex+11]}
+            keyHours = {'day' : "Saturday",
+                        'open' : True,
+                        'hours' : actualHours}
+            hoursArray.append(keyHours)
+        except:
+            keyHours = {'day' : "Saturday",
+                        'open' : False}
+            hoursArray.append(keyHours)
+    if sundayIndex == -1:
+        keyHours = {'day' : "Sunday",
+                        'open' : False}
+        hoursArray.append(keyHours)
+    else:
+        try:
+            day = hoursString[sundayIndex+7:mondayIndex]
+            if day.find("los") != -1: #check if the world close or Close or closed or Closed or Closure etc.. appears
+                keyHours = {'day' : "Sunday",
+                        'open' : False}
+                hoursArray.append(keyHours)
+            firstDigitIndex = [x.isdigit() for x in day].index(True)
+            actualHours = { 'open' : day[firstDigitIndex:firstDigitIndex+5],
+                            'close' : day[firstDigitIndex+6:firstDigitIndex+11]}
+            keyHours = {'day' : "Sunday",
+                        'open' : True,
+                        'hours' : actualHours}
+            hoursArray.append(keyHours)
+        except:
+            keyHours = {'day' : "Sunday",
+                        'open' : False}
+            hoursArray.append(keyHours)
+    return hoursArray
+
+def get_aldi_data(lat, lng):
+    API_URL = "https://www.aldi.co.uk/api/store-finder/search"
+    params = {  'latitude' : lat,
+                'longitude' : lng,
+                'fromMultipleBranch' : False
+             }
+    rq = requests.get(API_URL, params=params)
+    if rq.status_code != 200:
+        print(rq.json())
+        return False
+    try:
+        res = rq.json()["results"]
+        if len(res) == 0:
+            return False
+    except:
+        return False
+    i=0
+    intermediaryDayKeys = { 'Mon' : 0, 'Tue' : 1, 'Wed' : 2, 'Thu' : 3, 'Fri' : 4, 'Sat' : 5, 'Sun' : 6}
+    dayKeys = { 0 : 'Monday', 1 : 'Tuesday', 2 : 'Wednesday', 3 : 'Thursday', 4 : 'Friday', 5 : 'Saturday', 6 : 'Sunday'}
+    #INSERT POTENTIAL CHECK THAT STORE MATCHES DESIRED STORENAME!! (e.g. res[0]["name"])
+    #e.g.:
+    # while True:
+    #     if name != res[i]["name"]:
+    #         i += 1
+    #     else:
+    #         break
+    openingHours = res[i]["openingTimes"]
+    hoursArray = []
+    for index in range(len(openingHours)):
+        key = intermediaryDayKeys[openingHours[index]['day']]  #turn key into int
+        actualHours = []
+        while len(hoursArray) < key:
+            #Incase store is closed on a day of the week and therefore not included in the list 
+            #(although this is unlikely for ALDI, this check was added for Sainsbury API since how they deal with closed days is unclear,
+            #whereas it is unlikely ALDI's API would miss a day, so this check could potentially be removed if desired, however is left in for an extra layer of safety)
+            closedDayHours = {  'day' : dayKeys[len(hoursArray)],
+                                'open' : False  }
+            hoursArray.append(closedDayHours)
+        if openingHours[index]['closed'] == False:
+            hoursString = html.unescape(openingHours[index]['hours'])
+            while len(hoursString) > 11: #incase there are multiple time slots encoded within the string (format is unknown hence the strange code)
+                timeSlot = { 'open' : hoursString[:5],
+                                'close' : hoursString[8:13]}
+                actualHours.append(timeSlot)
+                hoursString = hoursString[13:]
+                if len(hoursString) > 0:
+                    try:
+                        firstDigitIndex = [x.isdigit() for x in hoursString].index(True)
+                        hoursString = hoursString[firstDigitIndex:]
+                    except:
+                        break
+            keyHours = {'day' : dayKeys[key],
+                'open' : True,
+                'hours' : actualHours
+                }
+        else:
+            keyHours = {'day' : dayKeys[key],
+                'open' : False
+                }
+        
+        hoursArray.append(keyHours)
+    while len(hoursArray) < 7:
+            #Incase store is closed on a day of the week (at the end of the list/week) and therefore not included in the list
+            closedDayHours = {  'day' : dayKeys[len(hoursArray)],
+                                'open' : False  }
+            hoursArray.append(closedDayHours)
+    return hoursArray
+
+
+
+
+
+
+print(get_waitrose_data(lat,lng))
+
+
+
 
 
 
